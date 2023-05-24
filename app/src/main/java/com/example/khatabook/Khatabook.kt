5463798e9df3be6c997b359498ab0398cb5eda1f
HARSH <harshgaudani1414@gmail.com>
2023-05-24 11:28:02 +0530

    Initial commit

---
+++ app/src/main/java/com/example/khatabook/Khatabook.kt
@@ -0,0 +1,11 @@
+package com.example.khatabook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Khatabook : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_khatabook)
+    }
+}
